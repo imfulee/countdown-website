3f097d5358233c5224281d48cacba16151670c32
Free <free@lawsnote.com>
2022-11-25 18:45:02 +0800

    [feature] add progress bar

--- src/App.svelte
+++ src/App.svelte
@@ -5,23 +5,49 @@
   const final = DateTime.fromISO("2022-12-12T20:05:00+08:00").setZone(
     "Asia/Taipei"
   );
+  const started = DateTime.fromISO("2022-09-19T07:00:00+08:00").setZone(
+    "Asia/Taipei"
+  );
+  const startToFinal = final.diff(started, [
+    "days",
+    "hours",
+    "minutes",
+    "seconds",
+  ]);
 
   setInterval(() => (now = DateTime.now().setZone("Asia/Taipei")), 1000);
 
-  $: difference = final
-    .diff(now, ["years", "months", "days", "hours", "minutes", "seconds"])
+  const minutesMultiplier = 60;
+  const hoursMultiplier = minutesMultiplier * 60;
+  const daysMultiplier = hoursMultiplier * 24;
+
+  $: nowToFinal = final
+    .diff(now, ["days", "hours", "minutes", "seconds"])
     .toObject();
+  $: startToNow = now
+    .diff(started, ["days", "hours", "minutes", "seconds"])
+    .toObject();
+  $: progressPercentage =
+    ((startToNow.days * daysMultiplier +
+      startToNow.hours * hoursMultiplier +
+      startToNow.minutes * minutesMultiplier +
+      startToNow.seconds) /
+      (startToFinal.days * daysMultiplier +
+        startToFinal.hours * hoursMultiplier +
+        startToFinal.minutes * minutesMultiplier +
+        startToFinal.seconds)) *
+    100;
 </script>
 
 <main>
   <div class="hero min-h-screen">
-    <div class="hero-content text-center">
+    <div class="text-center">
       <div class="grid grid-flow-col gap-5 text-center auto-cols-max">
         <div
           class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content"
         >
           <span class="countdown font-mono text-5xl">
-            <span style="--value:{difference.days};" />
+            <span style="--value:{nowToFinal.days};" />
           </span>
           days
         </div>
@@ -29,7 +55,7 @@
           class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content"
         >
           <span class="countdown font-mono text-5xl">
-            <span style="--value:{difference.hours};" />
+            <span style="--value:{nowToFinal.hours};" />
           </span>
           hours
         </div>
@@ -37,7 +63,7 @@
           class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content"
         >
           <span class="countdown font-mono text-5xl">
-            <span style="--value:{difference.minutes};" />
+            <span style="--value:{nowToFinal.minutes};" />
           </span>
           min
         </div>
@@ -45,11 +71,16 @@
           class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content"
         >
           <span class="countdown font-mono text-5xl">
-            <span style="--value:{parseInt(difference.seconds)};" />
+            <span style="--value:{parseInt(nowToFinal.seconds)};" />
           </span>
           sec
         </div>
       </div>
+      <progress
+        class="progress progress-success w-100 mt-5"
+        value={progressPercentage.toFixed(2)}
+        max="100"
+      />
     </div>
   </div>
 </main>
